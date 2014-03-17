b8fafad03b1f86cdc51900d2e791bda4c75000db
maurosabatino <maurosabatino@gmail.com>
2014-03-17 12:08:11 +0100

    Primo Commit

--- src/Entita/Litologia.java
+++
@@ -1,7 +0,0 @@
-package Entita;
-
-public class Litologia {
-	String nome;
-	String proprieta_termiche;
-	String stato_fratturazione;
-}
--- src/Entità/Litologia.java
+++
@@ -1,7 +0,0 @@
-package Entità;
-
-public class Litologia {
-	String nome;
-	String proprieta_termiche;
-	String stato_fratturazione;
-}
